c18cb0b0303453789a13a54c0c7e73bf7391e68c
ojcarrillo <ojcarrillo@yahoo.com>
2018-03-07 13:28:51 -0500

    agregado funcionalidad generar datos

--- src/main/java/com/example/demo/CronHilton.java
+++ src/main/java/com/example/demo/CronHilton.java
@@ -11,6 +11,7 @@ import java.time.format.DateTimeFormatter;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -19,70 +20,27 @@ public class CronHilton {
 
 	private final Logger log = LoggerFactory.getLogger(this.getClass());
 	
+	@Autowired
+	private GeneraRespuestas genRtas;
+	
 	/* ruta del directorio compartido */
 	private static final String PATH = File.separator + "ftp" + File.separator + "touresbalon" + File.separator;
-	private static final String PREFIJO = "rta_";
-
-	/* formato para la fecha hora de ejecucion */
-	private static final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+	private static final String EXT = ".txt";
 
 	@Scheduled(fixedDelay = 5000, initialDelay = 5000)
-	public void comprobarDirectorio() {
-		log.info("aca esta! " + dateTimeFormatter.format(LocalDateTime.now()));
-		log.info(PATH);
-		leerDirectorio();
-	}
-
 	public void leerDirectorio() {
 		File folder = new File(PATH);
 		if (folder.exists()) {
 			File[] files = folder.listFiles();
 			for (File file : files) {
-				if (file.isFile()) {
-					log.info(file.getName());
-					routingAccion(file.getName());
+				if (file.isFile() && file.getName().toLowerCase().endsWith(EXT)) {
+					System.out.println(file.getName());
+					genRtas.crearArchivoRta(file.getName());
 				}
 			}
-		} else {
-			log.error("no existe >>> " + PATH);
+		} else {			
+			log.error("===>>> directorio no existe >>> " + PATH);
 		}
 	}
 
-	public void routingAccion(String fileName) {
-		if (fileName.startsWith("query_reservas")) {
-			crearArchivoRta(fileName);
-		} else if (fileName.startsWith("query_servicios")) {
-
-		} else if (fileName.startsWith("query_compras")) {
-
-		} else if (fileName.startsWith("reservar")) {
-
-		} else if (fileName.startsWith("comprar")) {
-
-		} else if (fileName.startsWith("cancelar")) {
-
-		} else if (fileName.startsWith("cambiar")) {
-
-		}
-	}
-
-	public void crearArchivoRta(String fileName) {
-		fileName = PATH + PREFIJO + fileName;
-		File file = new File(fileName);
-		if (!file.exists()) {
-			try {
-				file.createNewFile();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-		/* agrega el nuevo mensaje al archivo */
-		try (BufferedWriter writer = Files.newBufferedWriter(Paths.get(fileName), StandardOpenOption.APPEND)) {
-			writer.write("respuesta " + dateTimeFormatter.format(LocalDateTime.now()));
-			writer.newLine();
-			writer.close();
-		} catch (IOException ioe) {
-			log.error("IOException: %s%n", ioe);
-		}
-	}
 }
---
+++ src/main/java/com/example/demo/GenerarDatosUtils.java
@@ -0,0 +1,125 @@
+package com.example.demo;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+import java.util.Random;
+
+import org.apache.commons.lang.StringUtils;
+import org.springframework.stereotype.Component;
+
+@Component
+public class GenerarDatosUtils {
+
+	private final static List<String> ciudades = Arrays.asList(
+			"Puerto Inírida,San José del Guaviare,Neiva,Riohacha,Santa Marta,Villavicencio,Pasto,Cúcuta,Mocoa,Armenia,Pereira,San Andres,Bucaramanga,Sincelejo,Ibagué,Cali,Mitú,Puerto Carreño,Leticia,Medellín,Arauca,Barranquilla,Cartagena,Tunja,Manizales,Florencia,Yopal,Popayán,Valledupar,Quibdó,Montería,Bogotá"
+					.split(","));
+	private final static String FORMATO_FECHA_HORA = "ddMMyyyyHH00";
+	private final static String FORMATO_FECHA_ENTRADA = "ddMMyyyy";
+	private final static SimpleDateFormat dateFormat = new SimpleDateFormat(FORMATO_FECHA_HORA);
+	private final static String LETRAS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+	private final static Integer TOTAL_PISOS = 9;
+	private final static Integer TOTAL_HABITACIONES = 20;
+	private final static Integer TOTAL_COMPRAS = 5;
+	private final static Double IVA = 0.71D; // 29%
+	private final static Integer FACTOR_PRECIO = 30;
+	private final static Double FACTOR_PRECIO_DINERO = 7000D;
+
+	public String getFechaDisponible(Date beginTime, Date endTime) {
+		return dateFormat.format(new Date(getRandomTimeBetweenTwoDates(beginTime, endTime)));
+	}
+
+	public String getCiudad() {
+		return StringUtils.rightPad(ciudades.get(new Random().nextInt(ciudades.size()) + 1), 21, " ");
+	}
+
+	public String getHabitacionDisponible() {
+		return  (new Random().nextInt(TOTAL_PISOS) + 1) + String.format("%02d", new Random().nextInt(TOTAL_HABITACIONES) + 1);
+	}
+
+	public Integer getHabitacionesComprados() {
+		return new Random().nextInt(TOTAL_COMPRAS) + 1;
+	}
+
+	public Date getFechaEntrada(String fecha) {
+		try {
+			return new SimpleDateFormat(FORMATO_FECHA_ENTRADA).parse(fecha);
+		} catch (ParseException e) {
+			return null;
+		}
+	}
+
+	public String getIdHabitacion() {
+		return generateRandomStr(LETRAS, 3) + String.format("%06d", new Random().nextInt(100000) + 1);
+	}
+	
+	public String getIdCancelacion() {
+		return String.format("%07d", new Random().nextInt(1000000) + 1);
+	}
+
+	public String getFiltros(File file) throws IOException {
+		BufferedReader br = new BufferedReader(new FileReader(file));
+		String datos = br.readLine();
+		br.close();
+		return datos;
+	}
+	
+	public List<String> getFiltrosVarios(File file) throws IOException {
+		BufferedReader br = new BufferedReader(new FileReader(file));
+		String opc = null;
+		List<String> datos = new ArrayList<String>();
+		while ((opc = br.readLine()) != null) {
+			datos.add(opc);
+		}
+		br.close();
+		return datos;
+	}
+
+	private String generateRandomStr(String aToZ, Integer size) {
+		Random rand = new Random();
+		StringBuilder res = new StringBuilder();
+		for (int i = 0; i < size; i++) {
+			int randIndex = rand.nextInt(aToZ.length());
+			res.append(aToZ.charAt(randIndex));
+		}
+		return res.toString();
+	}
+
+	private Long getRandomTimeBetweenTwoDates(Date beginTime, Date endTime) {
+		long diff = endTime.getTime() - beginTime.getTime() + 1;
+		return beginTime.getTime() + (long) (Math.random() * diff);
+	}
+
+	public Double getValorPto(Integer cantPtos) {
+		Double valorPto = (new Random().nextInt(FACTOR_PRECIO) + 1D) * FACTOR_PRECIO_DINERO;
+		return Math.floor(cantPtos * valorPto);
+	}
+
+	public Double getValorIVA(Double valorTotal) {
+		return valorTotal - (valorTotal * IVA);
+	}
+
+	public String getValoresMonetarios() {
+		Integer cantPuestos = getHabitacionesComprados();
+		Double valorPuestos = getValorPto(cantPuestos);
+		Double valorIVA = getValorIVA(valorPuestos);
+		return StringUtils.leftPad(String.valueOf(cantPuestos), 2, "0")
+				+ StringUtils.leftPad(String.valueOf(valorPuestos), 17, "0")
+				+ StringUtils.leftPad(String.valueOf(valorIVA), 17, "0");
+	}
+	
+	public String getValoresMonetarios(Integer cantPuestos) {
+		Double valorPuestos = getValorPto(cantPuestos);
+		Double valorIVA = getValorIVA(valorPuestos);
+		return StringUtils.leftPad(String.valueOf(cantPuestos), 2, "0")
+				+ StringUtils.leftPad(String.valueOf(valorPuestos), 17, "0")
+				+ StringUtils.leftPad(String.valueOf(valorIVA), 17, "0");
+	}
+}
